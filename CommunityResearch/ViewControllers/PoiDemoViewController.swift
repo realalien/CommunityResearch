99a537f89784d45e6b37296349bcc2d136e62de6
Zhu Jia Cheng <realalien@gmail.com>
2015-05-22 14:49:39 +0800

    cleaned code

--- CommunityResearch/ViewControllers/PoiDemoViewController.swift
+++ CommunityResearch/ViewControllers/PoiDemoViewController.swift
@@ -7,8 +7,6 @@
 //
 
 
-
-
 import UIKit
 
 class PoiDemoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, BMKPoiSearchDelegate, BMKGeneralDelegate, BaiduLbsDataProviderDelegate{
@@ -17,29 +15,20 @@ class PoiDemoViewController: UIViewController, UITableViewDelegate, UITableViewD
  
     var poiSearcher = BMKPoiSearch()
     var centerlocation: CLLocationCoordinate2D!
-    var poiResults: NSMutableArray!
-    var searchKeyword: String!
-    
     
     override func viewDidLoad() {
         poiSearcher.delegate = self
-        poiResults = NSMutableArray()
-        searchKeyword = ""
     }
-    
-    
     
     // search controller result table delegate
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
     
-    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         return UITableViewCell()
     }
-    
     
     // searchbar delegate
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
@@ -49,18 +38,14 @@ class PoiDemoViewController: UIViewController, UITableViewDelegate, UITableViewD
         }
         
         println("\(searchText)")
-        
-        
         var datap = BaiduLbsDataProvider()
         datap.delegate = self;
         datap.searchNearbyLocation(searchText, centerCoord: CLLocationCoordinate2DMake(31.224349, 121.476753) )  //  TODO: centerlocation
-        
     }
-    
     
     // BaiduLbsDataProviderDelegate
     func finishedResults(results:[BMKPoiInfo]){
-        NSLog("Done!")
+        NSLog("Done! num of results  \(count(results))")
     }
     
 }
